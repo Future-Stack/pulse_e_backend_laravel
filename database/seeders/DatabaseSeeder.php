9af7f5884ca12d0171f545c223ee806994f470ed
Md. Mahbubur Rahman <mahbubur@softvence.com>
2026-08-18 08:06:16 +0600

    providers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             HealthGoalSeeder::class,
             LifeJourneySeeder::class,
             LifeStageSeeder::class,
+            MetroSeeder::class,
             ProviderTaxonomySeeder::class,
         ]);
     }
